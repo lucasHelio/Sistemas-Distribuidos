824939a8f0e044c27267553310db86b82fb4fa72
lucasHelio <70375650+lucasHelio@users.noreply.github.com>
2023-05-30 19:12:35 -0300

    Update passivo.py

--- Lab 3/passivo.py
+++ Lab 3/passivo.py
@@ -73,144 +73,15 @@ class Dic(rpyc.Service):
         ret = sorted(palavras)
         return ret
 
-    
-    def exposed_printaChave(self, chave):
-        arquivoLeitura()
-        for x in Dicionario:
-            if chave == x.chave:
-                y = ("\nPalavra: "+ x.chave+ "\t\tDefinicao: ")
-                for d in x.valores:
-                    y = y +str(d)
-                y= y+"\n"
-                return y
-        msg = "\npalavra nao encontrada\n"
-        return msg
-    
-    def exposed_adicionaChave(self, chave, valor):
-        arquivoLeitura()
-        if not encontraChave(chave):
-            Dicionario.append(Palavra(chave, valor+"; ")) 
-            arquivoEscrita()  
-            msg = "\nPalavra adicionada com sucesso!\n"
-        else:
-            msg = "\nA palavra ja existe\n"
-        return msg
-    
-    def exposed_mostraDefinicoes(self, chave):
+    def exposed_removePalavra(self, chave):
         Obj = encontraChave(chave)
         if Obj:
-            y=''
-            for x in Obj.valores:
-                y = y + str(Obj.valores.index(x))+ " - "+str(x)+"\n"
-            return y #temos que retornar a msg
-        return 0
-
-    def exposed_alteraValorChave(self, chave, idOldDef, novaDefinicao):
-        Obj = encontraChave(chave)
-        if Obj:
-            valorN = idOldDef
-            if Obj.valores[int(valorN)] != "":
-                Obj.valores.pop(int(valorN))
-                Obj.valores.insert(int(valorN), novaDefinicao+"; ")
-                msg = "\nA definicao foi alterada\n"
-            else:
-                msg = "\nDefinicao escolhida nao existe\n"
-        else:
-            msg = "\nA palavra nao foi encontrada\n" 
-        return msg
-    
-    def exposed_adicionaValor(self, chave, valor):
-        Obj = encontraChave(chave)
-        if Obj:
-            novoValor = valor
-            Obj.valores.append(novoValor + "; ")
-            msg = "\nO valor foi adicionado com sucesso!\n"    
+            Dicionario.remove(Obj)
+            arquivoEscrita()
+            msg = "\nA palavra foi removida\n"    
         else:
             msg = "\nA palavra nao foi encontrada\n"
         return msg
-    
-
-
-
-#arquivoLeitura()
-srv = ForkingServer(Dic, port = PORT)
-srv.start()
-
-    
-#passivo
-
-import rpyc
-
-Dicionario = []
-
-# define a localizacao do servidor
-
-PORT = 5000 # porta de acesso
-
-from rpyc.utils.server import ForkingServer
-
-def arquivoEscrita():
-    Arquivo = open("dicionario.txt","w")
-    for x in Dicionario:
-        Arquivo.write(str(x.chave))
-        for y in x.valores:
-            Arquivo.write("*"+ str(y))
-        Arquivo.write("\n")
-            
-    Arquivo.close()
-    return
-
-def arquivoLeitura():
-    Arquivo = open("dicionario.txt", "r+")
-    x = Arquivo.read().splitlines()
-    for a in x:
-        i = a.split('*')
-        x=1
-        for x in i[1:]:
-            adicionaChaveServidor(i[0], x)
-            
-    Arquivo.close()
-    return
-
-def adicionaChaveServidor(chave, valor):
-    if not encontraChave(chave):
-        Dicionario.append(Palavra(chave, valor))   
-        return
-    else:
-        return
-
-
-
-class Palavra:
-    def __init__(self, chave, valor):
-        self.chave = chave
-        self.valores = [valor]
-
-
-
-
-def encontraChave(chave):
-        for x in Dicionario:
-            if chave == x.chave:
-                return x
-        return 0
-
-
-
-class Dic(rpyc.Service):
-    def on_connect(self, conn):
-        print("Conexao iniciada: ")
-    
-    def on_disconnect(sel, conn):
-        print("Conexão finalizada")
-    
-    def exposed_listaPalavras(self):
-        arquivoLeitura()
-        palavras = []
-        for x in Dicionario:
-            palavras.append(x.chave)
-        ret = sorted(palavras)
-        return ret
 
     
     def exposed_printaChave(self, chave):
@@ -236,6 +107,7 @@ class Dic(rpyc.Service):
         return msg
     
     def exposed_mostraDefinicoes(self, chave):
+        arquivoLeitura()
         Obj = encontraChave(chave)
         if Obj:
             y=''
@@ -245,12 +117,14 @@ class Dic(rpyc.Service):
         return 0
 
     def exposed_alteraValorChave(self, chave, idOldDef, novaDefinicao):
+        arquivoLeitura()
         Obj = encontraChave(chave)
         if Obj:
             valorN = idOldDef
             if Obj.valores[int(valorN)] != "":
                 Obj.valores.pop(int(valorN))
                 Obj.valores.insert(int(valorN), novaDefinicao+"; ")
+                arquivoEscrita()
                 msg = "\nA definicao foi alterada\n"
             else:
                 msg = "\nDefinicao escolhida nao existe\n"
@@ -263,6 +137,7 @@ class Dic(rpyc.Service):
         if Obj:
             novoValor = valor
             Obj.valores.append(novoValor + "; ")
+            arquivoEscrita()
             msg = "\nO valor foi adicionado com sucesso!\n"    
         else:
             msg = "\nA palavra nao foi encontrada\n"
