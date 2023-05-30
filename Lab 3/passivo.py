893d5302a6dd178f52461b771db7e90a3c0985cf
lucasHelio <70375650+lucasHelio@users.noreply.github.com>
2023-05-30 13:36:08 -0300

    Update passivo.py

--- Lab 3/passivo.py
+++ Lab 3/passivo.py
@@ -2,6 +2,7 @@
 
 import rpyc
 
+Dicionario = []
 
 # define a localizacao do servidor
 
@@ -26,7 +27,10 @@ def arquivoLeitura():
     x = Arquivo.read().splitlines()
     for a in x:
         i = a.split('*')
-        adicionaChaveServidor(i[0], i[1:])
+        x=1
+        for x in i[1:]:
+            adicionaChaveServidor(i[0], x+"; ")
+            
     Arquivo.close()
     return
 
@@ -37,7 +41,7 @@ def adicionaChaveServidor(chave, valor):
     else:
         return
 
-Dicionario = []
+
 
 class Palavra:
     def __init__(self, chave, valor):
@@ -55,7 +59,7 @@ def encontraChave(chave):
 
 
 
-class Echo(rpyc.Service):
+class Dic(rpyc.Service):
     def on_connect(self, conn):
         print("Conexao iniciada: ")
     
@@ -63,17 +67,19 @@ class Echo(rpyc.Service):
         print("Conexão finalizada")
     
     def exposed_listaPalavras(self):
-        palavras = " "
+        palavras = []
         for x in Dicionario:
-            palavras = palavras +"\n"+ x.chave
-        return palavras
+            palavras.append(x.chave)
+        ret = sorted(palavras)
+        return ret
+
     
     def exposed_printaChave(self, chave):
         for x in Dicionario:
             if chave == x.chave:
                 y = ("\nPalavra: "+ x.chave+ "\t\tDefinicao: ")
                 for d in x.valores:
-                    y = y +d
+                    y = y +str(d)
                 y= y+"\n"
                 return y
         msg = "\npalavra nao encontrada\n"
@@ -92,7 +98,7 @@ class Echo(rpyc.Service):
         if Obj:
             y=''
             for x in Obj.valores:
-                y = y + str(Obj.valores.index(x))+ " - "+x+"\n"
+                y = y + str(Obj.valores.index(x))+ " - "+str(x)+"\n"
             return y #temos que retornar a msg
         return 0
 
@@ -126,5 +132,147 @@ class Echo(rpyc.Service):
 
 
 if __name__ == "__main__":
-    srv = ForkingServer(Echo, port = PORT)
+    arquivoLeitura()
+    srv = ForkingServer(Dic, port = PORT)
     srv.start()
+    arquivoEscrita()
+    
+#passivo
+
+import rpyc
+
+Dicionario = []
+
+# define a localizacao do servidor
+
+PORT = 5000 # porta de acesso
+
+from rpyc.utils.server import ForkingServer
+
+def arquivoEscrita():
+    Arquivo = open("dicionario.txt","w")
+    for x in Dicionario:
+        Arquivo.write(str(x.chave))
+        for y in x.valores:
+            Arquivo.write("*"+ str(y))
+        Arquivo.write("\n")
+            
+    Arquivo.close()
+    return
+
+def arquivoLeitura():
+    Arquivo = open("dicionario.txt", "r+")
+
+    x = Arquivo.read().splitlines()
+    for a in x:
+        i = a.split('*')
+        x=1
+        for x in i[1:]:
+            adicionaChaveServidor(i[0], x+"; ")
+            
+    Arquivo.close()
+    return
+
+def adicionaChaveServidor(chave, valor):
+    if not encontraChave(chave):
+        Dicionario.append(Palavra(chave, valor))   
+        return
+    else:
+        return
+
+
+
+class Palavra:
+    def __init__(self, chave, valor):
+        self.chave = chave
+        self.valores = [valor]
+
+
+
+
+def encontraChave(chave):
+        for x in Dicionario:
+            if chave == x.chave:
+                return x
+        return 0
+
+
+
+class Dic(rpyc.Service):
+    def on_connect(self, conn):
+        print("Conexao iniciada: ")
+    
+    def on_disconnect(sel, conn):
+        print("Conexão finalizada")
+    
+    def exposed_listaPalavras(self):
+        palavras = []
+        for x in Dicionario:
+            palavras.append(x.chave)
+        ret = sorted(palavras)
+        return ret
+
+    
+    def exposed_printaChave(self, chave):
+        for x in Dicionario:
+            if chave == x.chave:
+                y = ("\nPalavra: "+ x.chave+ "\t\tDefinicao: ")
+                for d in x.valores:
+                    y = y +str(d)
+                y= y+"\n"
+                return y
+        msg = "\npalavra nao encontrada\n"
+        return msg
+    
+    def exposed_adicionaChave(self, chave, valor):
+        if not encontraChave(chave):
+            Dicionario.append(Palavra(chave, valor+"; "))   
+            msg = "\nPalavra adicionada com sucesso!\n"
+        else:
+            msg = "\nA palavra ja existe\n"
+        return msg
+    
+    def exposed_mostraDefinicoes(self, chave):
+        Obj = encontraChave(chave)
+        if Obj:
+            y=''
+            for x in Obj.valores:
+                y = y + str(Obj.valores.index(x))+ " - "+str(x)+"\n"
+            return y #temos que retornar a msg
+        return 0
+
+    def exposed_alteraValorChave(self, chave, idOldDef, novaDefinicao):
+        Obj = encontraChave(chave)
+        if Obj:
+            #y=''
+            #for x in Obj.valores:
+            #    y = y + str(Obj.valores.index(x))+ " - "+x+"*"
+            valorN = idOldDef
+            if Obj.valores[int(valorN)] != "":
+                Obj.valores.pop(int(valorN))
+                Obj.valores.insert(int(valorN), novaDefinicao+"; ")
+                msg = "\nA definicao foi alterada\n"
+            else:
+                msg = "\nDefinicao escolhida nao existe\n"
+        else:
+            msg = "\nA palavra nao foi encontrada\n" 
+        return msg
+    
+    def exposed_adicionaValor(self, chave, valor):
+        Obj = encontraChave(chave)
+        if Obj:
+            novoValor = valor
+            Obj.valores.append(novoValor + "; ")
+            msg = "\nO valor foi adicionado com sucesso!\n"    
+        else:
+            msg = "\nA palavra nao foi encontrada\n"
+        return msg
+    
+
+
+if __name__ == "__main__":
+    arquivoLeitura()
+    srv = ForkingServer(Dic, port = PORT)
+    srv.start()
+    arquivoEscrita()
+    
