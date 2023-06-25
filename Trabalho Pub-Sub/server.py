1efbec8edf455d92673380369611d4be5e0794c3
lucasHelio <70375650+lucasHelio@users.noreply.github.com>
2023-06-25 19:53:20 -0300

    Update server.py

--- Trabalho Pub-Sub/server.py
+++ Trabalho Pub-Sub/server.py
@@ -1,93 +1,64 @@
+import os
+import pickle
+import sys
+import rpyc
 from interface import BrokerService, Content
 from rpyc.utils.server import ThreadedServer
-#import pickle
+import atexit
+from threading import Thread
 
 
 PORTA = 10001
-
-LISTAID = []
-LISTATOPICOS = []
 BUFFERSIZE = 10
 
-"""
-dicionario
--chave = topico
--valor = inscrito
 
-{topico1: eu, lucas, di,
- topico2: lucas
- topico3: 
- topicox: []}
-
-listavazia = []
-LISTATOPICOS[topix] = listavazia
-
-LISTATOPICOS[topix].append(inscrotp)
-
-LISTATOPICOS[topix].remove(inscrito)
-
-
-topico1:
-    obj1:
-        id: usuario
-        --address: client_address
-        --func: callback
-        listContent: Jogo
-                     Autor
-                     novo champ
-
-                     Esporte
-                     Autor 2
-                     Time venceu
-
-                     [max = 10]
+class Usuario:
+    def __init__(self, nome):
+        self.nome = nome
+        self.posts = []
     
-    obj2:
-        id: j
-        address: client_address2
-        func: callback
 
+def adicionaPost(listaPost, post):
+    if len(listaPost) <10:
+        listaPost.append(post)
+    else:
+        listaPost.pop(0)
+        listaPost.append(post)
+    return 
 
-{topico1: obj1, obj2, obj3,
- topico2: obj5,
- topico3: 
- topicox: []}
-
-
-
-"""
-
-class userFunc:
-    def __init__(self, id):
-        self.id = id
-        #self.func = callback
-        self.listContent = []
-
-
-def escreveArquivo(Lista, arquivo):
-    f1 = open(str(arquivo) +".txt", "w")
-    f1.write(Lista)
-    f1.close()
+def removePrimeiroPost(listaPost):
+    listaPost.pop(0)
     return
 
-def lerArquivo(Lista, arquivo):
-    try:
-        f1 = open(arquivo+".txt", "r")
-        Lista = f1.read()
-        f1.close()
-    except FileNotFoundError:
-        # Arquivo não encontrado, cria um novo arquivo vazio
-        f1 = open(arquivo + ".txt", "a+")
-        f1.close()
-        Lista = ""  # Lista vazia
-    return Lista
 
-def create_post(id, topic, data):
-    novoPost = Content()
-    novoPost.author = id
-    novoPost.topic = topic
-    novoPost.data = data
-    return novoPost
+
+DICTUSERTOP = []
+'''DICTUSERTOP
+    Usuario user:
+'''
+
+DICTCONECTADOS = {}
+
+"""DICTCONECTADOS
+    key: id
+    value: client_address
+"""
+
+DICTIDCALLBACK = {}
+
+"""DICTIDCALLBACK
+    key: id
+    value: callback func
+"""
+
+DICTTOPIC = {}
+
+"""DICTTOPIC
+    key: topico
+    value: lista de dict:
+                        key: id
+                        value: listContent
+"""
 
 
 class BrokServ(BrokerService):
@@ -106,91 +77,177 @@ class BrokServ(BrokerService):
         client_address = self.client_addresses.pop(conn, None)
         if client_address is not None:
             print("Conexao finalizada:", client_address[0] + ":" + str(client_address[1]))
-            client_id = None
-            for id, values in LISTAID.items():
-                if values["client_address"] == client_address:
-                    client_id = id
-                    break
-            if client_id is not None:
-                del LISTAID[client_id]
+            for key, val in DICTCONECTADOS.items():
+                if val == client_address:
+                    user = key
+            del DICTCONECTADOS[user]
 
-
+#---------------------------------------------------------
     def create_topic(self, UserId, topicname):
         listaInscritos = []
-        LISTATOPICOS[topicname] = listaInscritos
+        DICTTOPIC[topicname] = listaInscritos
+#---------------------------------------------------------
 
     def exposed_login(self, id, callback):
-        """if id in LISTAID:
-            client_address = self.current_client_address
-            LISTAID[id] = {"callback": callback, "client_address": client_address}
-            return True"""
         try:
             client_address = self.current_client_address
-            LISTAID[id] = {"callback": callback, "client_address": client_address}
-            #chamar o FnNotify tambem
-            for topico in LISTATOPICOS:
-                if topico.id == id:
-                    if len(topico.listContent) > 0:
-                        func = LISTAID[topico.id]["callback"]
-                        func(topico.listContent) #Como fazer para o cliente rodar?
-                        topico.listContent.clear()
+            DICTCONECTADOS[id] = client_address
+            DICTIDCALLBACK[id] = rpyc.async_(callback)
+            for usuario in DICTUSERTOP:
+                if id == usuario.nome:
+                    listatemporaria = []
+                    while len(usuario.posts)!= 0:
+                        listatemporaria.append(usuario.posts[0])
+                        removePrimeiroPost(usuario.posts)
+                    callback(listatemporaria)
+                return True
+            
+            novoUsuario = Usuario(id)
+            DICTUSERTOP.append(novoUsuario)
             return True
         
-        except:
+        except Exception as error:
+            print(error)
             return False
 
 
     def exposed_list_topics(self):
-        return LISTATOPICOS.keys()
+        return DICTTOPIC.keys()
+
+    def exposed_publish(self, id, topico, info):
+        try:
+            novoPost = Content(author=id, topic=topico, data=info)
+
+            for user in DICTTOPIC[topico]:
+                if(user in DICTCONECTADOS):
+                    func = DICTIDCALLBACK[user]
+                    func([novoPost])
+                else:     
+                    for usuario in DICTUSERTOP:
+                        if usuario.nome == user:
+                            adicionaPost(usuario.posts, novoPost)
+
+                    dictUser = DICTTOPIC[topico]
+                    dictUser[user].append(novoPost)
+                    DICTTOPIC[topico] = dictUser
+            return True
+        
+        except Exception as error:
+            print("ERRO - Tópico "+ error+" inserido não existe")
+            return False        
 
 
-    def exposed_publish(self, id, topic, data):
-        novoPost = create_post(id, topic, data)
-        for topico in LISTATOPICOS[topic]:
-            topico.listContent.append(novoPost)
-            if(topico.id in LISTAID):
-                func = LISTAID[topico.id]["callback"]
-                func(topico.listContent) #Como fazer para o cliente rodar?
-                topico.listContent.clear()
-                
-        #pensar em como saber se o usuario recebeu o callback ou não
-        #Agora buscar todos os inscritos e
-        #chamar FnNotify para cada um inscrito com novoPost
-        #pass
 
     def exposed_subscribe_to(self, id, topic):
-        if topic in LISTATOPICOS:
-            for x in LISTATOPICOS[topic]:
-                if id == x.id: return False
-            
-            novoUser = userFunc(id)
-            LISTATOPICOS[topic].append(novoUser)
+
+        if topic in DICTTOPIC:
+
+            for userContent in DICTTOPIC[topic]:
+                if id == userContent: return False
+
+            dictUser = DICTTOPIC[topic]
+            dictUser.update({id:[]})
+            DICTTOPIC[topic] = dictUser
+
+
             return True
+        
         return False
 
     def exposed_unsubscribe_to(self, id, topic):
-        if topic in LISTATOPICOS:
-            for x in LISTATOPICOS[topic]:
-                if id == x.id: 
-                    LISTATOPICOS[topic].remove(x)
-                    return True
+        try:
+            if topic in DICTTOPIC:
+                dictUser = DICTTOPIC[topic]
+                del dictUser[id]
+                DICTTOPIC[topic] = dictUser
+    
+                return True
+        except KeyError:
+            print('Algo deu errado')
         return False
-        
+
+
+
+#---------------------------------------------------------   
+def load(dictionary, nome):
+    try: 
+        with open(str(nome)+'.pickle', 'rb') as f: 
+            loadDict = pickle.load(f)
+            print("Dicionario carregado")
+            dictionary.update(loadDict)
+    except FileNotFoundError: 
+        print("Dicionario novo criado")
+
+def save(dictionary, nome):
+	with open(str(nome)+'.pickle', 'wb') as f: 
+		pickle.dump(dict(dictionary), f)
+		print("Dicionario salvo")
+	return
+
+def save_on_exit():
+    save(DICTTOPIC, 'DICTTOPIC')
+#---------------------------------------------------------
+
+def selecionar_opcao(srv):
+    while True:
+        print("")
+        print("OPCOES:")
+        print("1 - Listar Topicos")
+        print("2 - Criar Topico")
+        print("3 - Deletar Topico")
+        print("\'fim\' - Desliga servidor")
+        selecionado = input("Selecionar opcao: ")
+        if selecionado == '1':
+            listaTopicos = DICTTOPIC.keys()
+            print("╔═════════════╗")
+            print("║   TOPICOS   ║")
+            print("╚═════════════╝")
+            for topico in listaTopicos: print("║ "+topico)
+        if selecionado == '2':
+            new_topic = input("Qual o Topico a ser criado? ")
+            DICTTOPIC[new_topic] = dict()
+        if selecionado == '3':
+            delete_topic = input("Qual o Tópico a ser deletado? ")
+            if delete_topic in DICTTOPIC:
+                del DICTTOPIC[delete_topic]
+                print("Tópico", delete_topic, "foi deletado.")
+            else:
+                print("O Tópico", delete_topic, "não existe.")
+        if selecionado == 'fim':
+            print("Interrompendo servidor...")
+            srv.close()
+            break
 
 def main():
 
-    global LISTATOPICOS 
-    global LISTAID
+    global DICTCONECTADOS
+    global DICTIDCALLBACK
+    global DICTTOPIC 
 
-    
-    srv = ThreadedServer(BrokServ, port=PORTA)
-    lerArquivo(LISTATOPICOS, "Topicos")
-    lerArquivo(LISTATOPICOS, "Id")
-    
-    srv.start()
+    load(DICTTOPIC, 'DICTTOPIC')
 
-    escreveArquivo(LISTATOPICOS, "Topicos")
-    escreveArquivo(LISTAID, "Id")
+    srv = ThreadedServer(BrokServ, port=PORTA, protocol_config={'allow_public_attrs':True})
     
+    print("Comecando o Broker...")
+    print("╔═════════════╗")
+    print("║   TOPICOS   ║")
+    print("╚═════════════╝")
+    for topico in DICTTOPIC: print("║ "+topico)
 
-main()
+    selecionar_opcao_thread = Thread(target=selecionar_opcao, args=(srv,))
+    selecionar_opcao_thread.daemon = True
+    selecionar_opcao_thread.start()
+
+    atexit.register(save_on_exit)
+    
+    try:
+        srv.start()
+    except KeyboardInterrupt:
+        save_on_exit()
+        sys.exit(0)
+    except Exception as error:
+        srv.close()
+        save_on_exit()
+
+if __name__ == "__main__":
+    main()
