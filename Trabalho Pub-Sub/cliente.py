7687c8b1a3afb7b94a0a5c8a41654bf6d1cc6d0c
lucasHelio <70375650+lucasHelio@users.noreply.github.com>
2023-06-25 19:53:35 -0300

    Update cliente.py

--- Trabalho Pub-Sub/cliente.py
+++ Trabalho Pub-Sub/cliente.py
@@ -4,13 +4,13 @@ from interface import Content
 #endereco do servidor
 SERVER = 'localhost'
 PORTA = 10001
+user = ''
 
-
-def FnNotify(listOfContent):
+def printLC(listOfContent):
     for content in listOfContent:
         print()
         print("Autor:  " + content.author)
-        print("Topico: " +content.topic)
+        print("Topico: " + content.topic)
         print(content.data)
         print()
     return None
@@ -18,29 +18,37 @@ def FnNotify(listOfContent):
 
 def realizarLogin():
     user = str(input("Insira seu user: "))
-    isLogged = conn.root.login(user, FnNotify)
+    isLogged = conn.root.login(user, printLC)
     if(isLogged):
         return user
     else:
-        return "Erro ao Logar"
+        return ''
 
 def fazRequisicoes(conn):
+    global user
     user = realizarLogin()
-    if(user == ""):
+    if(user == ''):
         print("Usuario nao autorizado.")
         return
     while True:
+        print()
         print("Opcoes:")
-        print("\t0 - Sair")
-        print("\t1 - Listar topicos")
-        print("\t2 - Publicar em topico")
-        print("\t3 - Inscrever em um topico")
+        print("0 - Sair")
+        print("1 - Listar topicos")
+        print("2 - Publicar em topico")
+        print("3 - Inscrever em um topico")
+        print("4 - Desinscrever em um topico")
+        
         opcao = str(input("Selecione a opcao: "))
+        print()
         
         if opcao == "0": break
         
         if opcao == '1':
-            conn.root.list_topics()
+            listaTopicos = conn.root.list_topics()
+            print("Os Topicos são:")
+            for topico in listaTopicos: print(topico)
+            
         
         elif opcao == '2':
             topico = str(input("Topico da publicacao: "))
@@ -54,8 +62,6 @@ def fazRequisicoes(conn):
             topico = str(input("Inscrever-se no topico: "))
             if (conn.root.subscribe_to(user, topico)): print("Inscricao Concluida")
             else: print("Nao foi possivel realizar a inscricao")
-            #callback
-            #broker.root.subscribe_to(user, topico, callback)
         
         elif opcao == '4':
             topico = str(input("Deseja se desinscrever de qual topico? "))
@@ -64,19 +70,26 @@ def fazRequisicoes(conn):
             else:
                 print("Nao foi possivel desincrever-se do topico "+topico+
                 ". Ou voce nao estava inscrito nele, ou o topico nao existe.")
+        
+
         else:
             print("Opcao nao Reconhecida")
 
+
 def iniciaConexao():
     conn = rpyc.connect(SERVER, PORTA)
-    #print(conn.root.get_service_name())
     return conn
 
 def main():
+    
     global conn
+    global bgsrv
     conn = iniciaConexao()
+    bgsrv = rpyc.BgServingThread(conn)
     fazRequisicoes(conn)
+    bgsrv.stop()
     conn.close()
+    
 
 if __name__ == '__main__':
     main()
